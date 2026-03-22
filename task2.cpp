20f0ff58e2e3dda18f4671639bfa506acff3de12
Shlaev Konstantin <57225083+xkolx@users.noreply.github.com>
2026-03-22 18:09:36 +0300

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    cout << "Print numbers (print any letter to end input):" << endl;
+
+    list<int> L{ istream_iterator<int>(cin), istream_iterator<int>() };
+
+    if (L.empty()) {
+        cout << "The list is empty." << endl;
+        return 0;
+    }
+
+    auto i = L.begin(); 
+
+    while (i != L.end()) {
+        ++i; 
+        if (i != L.end()) {
+            i = L.erase(i); 
+        }
+    }
+
+
+    cout << "Modified list: ";
+    copy(L.begin(), L.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    return 0;
+}
