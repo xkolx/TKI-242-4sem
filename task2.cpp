6346ecf86f2fc7255113f689329a13c46cab1c5d
Shlaev Konstantin <57225083+xkolx@users.noreply.github.com>
2026-03-24 11:38:12 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -2,34 +2,33 @@
 #include <list>
 #include <iterator>
 #include <algorithm>
-
 using namespace std;
 
 int main() {
     cout << "Print numbers (print any letter to end input):" << endl;
-
+    
     list<int> L{ istream_iterator<int>(cin), istream_iterator<int>() };
-
+    
     if (L.empty()) {
         cout << "The list is empty." << endl;
-        return 0;
+        return 1;
     }
-
-    auto i = L.begin(); 
-
+    
+    L.reverse();
+    
+    auto i = L.begin();
     while (i != L.end()) {
-        ++i; 
+        ++i;
         if (i != L.end()) {
-            i = L.erase(i); 
+            i = L.erase(i);
         }
     }
-
-
+    
+    L.reverse();
+    
     cout << "Modified list: ";
-    copy(L.begin(), 
-        L.end(), 
-        ostream_iterator<int>(cout, " "));
+    copy(L.begin(), L.end(), ostream_iterator<int>(cout, " "));
     cout << endl;
-
+    
     return 0;
 }
